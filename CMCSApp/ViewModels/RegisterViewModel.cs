d69c7a072b80212a940d133a5b564564a2807522
Kuzivakwashe C Kanyemba <claseyk@gmail.com>
2025-11-16 16:08:11 +0200

    Implement a view model for registration

---
+++ CMCSApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CMCSApp.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        public string DisplayName { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please select a role.")]
+        public string SelectedRole { get; set; } = string.Empty;
+
+        // Convert roles to SelectListItem
+        public IEnumerable<SelectListItem> AvailableRoles { get; set; } = new List<SelectListItem>
+        {
+            new SelectListItem { Text = "Lecturer", Value = "Lecturer" },
+            new SelectListItem { Text = "Coordinator", Value = "Coordinator" },
+            new SelectListItem { Text = "Manager", Value = "Manager" }
+        };
+    }
+}
