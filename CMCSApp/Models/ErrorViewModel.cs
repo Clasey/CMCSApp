b63f8e26c7ec6c2d167f55bc5bbcb8925756f424
Kuzivakwashe C Kanyemba <claseyk@gmail.com>
2025-11-16 15:16:36 +0200

    Added ErrorViewModel for standardized error handling in MVC

--- CMCSApp/Models/ErrorViewModel.cs
+++ CMCSApp/Models/ErrorViewModel.cs
@@ -3,7 +3,6 @@ namespace CMCSApp.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
